91bd7052ddc0a9ac013f2a0ad36b872a68c71173
AJ-Ax-droid <mohanpandey8827@outlook.com>
2025-07-20 18:17:17 +0530

    Updated Credit View Screen

--- src/MakeTransaction/MakeCreditTransaction.jsx
+++ src/MakeTransaction/MakeCreditTransaction.jsx
@@ -1,10 +1,67 @@
-const MakeCreditTransaction = () => {
-  return (
-    <div>
-      <h1>Make Credit Transaction</h1>
-      <p>Form for making a credit transaction.</p>
-    </div>
-  );
-};
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import { styled } from '@mui/material/styles';
+import FormControl from '@mui/material/FormControl';
 
-export default MakeCreditTransaction;
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export default function MakeCreditTransaction() {
+  return (
+    <Box sx={{ minHeight: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Box
+        component="form"
+        sx={{ width: 400, height: 350, display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 3, p: 4, boxShadow: 3, borderRadius: 3, bgcolor: 'background.paper' }}
+        noValidate
+        autoComplete="off"
+      >
+        
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField
+          id="CreditTransactionUserName"
+          label="Enter Your Name"
+          variant="standard"
+          required
+        />
+          </Box>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <CurrencyRupeeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+          <TextField
+            required
+            id="CreditAmount"
+            label="Amount to Credit"
+            variant="standard"
+          />
+          
+
+
+        </Box >
+        <Button
+          sx={{ mt: 2, width: '60%' }}
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<AddIcon />}
+        >
+          Add Money
+        </Button>
+      </Box>
+    </Box>
+  );
+}
