26563a21db7fc976b2e6373b6fc0425d4f0378a4
AJ-Ax-droid <mohanpandey8827@outlook.com>
2025-07-28 02:05:26 +0530

    Update basename in main

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { UserProvider } from './UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/BankingWebApp/">
       <UserProvider>
         <App />
       </UserProvider>
