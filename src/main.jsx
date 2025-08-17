e73050e29320c21ec2846b7d691171ced71a554c
AJ-Ax-droid <mohanpandey8827@outlook.com>
2025-08-17 23:44:03 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,31 @@ import { BrowserRouter } from 'react-router-dom'
 import { CssVarsProvider } from '@mui/joy/styles';
 import { Login } from '@mui/icons-material'
 import { UserProvider } from './UserContext.jsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './CSS/App.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <CssVarsProvider>
+<ThemeProvider theme={theme}>
 
     <BrowserRouter basename="/BankingWebApp/">
       <UserProvider>
         <App />
       </UserProvider>
     </BrowserRouter>
+</ThemeProvider>
     </CssVarsProvider>
   // </StrictMode>,
 )
